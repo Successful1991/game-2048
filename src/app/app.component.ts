b84bbb7ce039ffd94f3ff32cfcdd601148f19008
Tsys Artem <19successful91@gmail.com>
2018-10-08 18:46:56 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public places: any[] = [2, '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+  constructor() {}
+  ngOnInit() {
+    this.addNumber();
+  }
+
+  addNumber() {
+    const number = this.random(1, 2) * 2;
+    const index = (this.random(1, 16) - 1);
+    this.searchEmptyCell(index, number);
+  }
+  searchEmptyCell(indexNewNumber, number) {
+    const emptyCell = this.places.indexOf('');
+    if (emptyCell === -1) {
+      console.log('GAME over');
+    } else if (this.places[indexNewNumber] === '') {
+      this.places[indexNewNumber] = number;
+    } else if (this.places[indexNewNumber] !== '' && indexNewNumber < this.places.length - 1 ) {
+      indexNewNumber -= 1;
+      this.searchEmptyCell(indexNewNumber, number);
+    } else {
+      indexNewNumber += 1;
+      this.searchEmptyCell(indexNewNumber, number);
+    }
+  }
+  random(min, max) {
+    return (Math.floor(Math.random() * (max - min)) + min);
+  }
+
+  swipe(action) {
+    console.log(action);
+  }
+  // swipe(e) {
+  //   console.log(e);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+    'swipe': {velocity: 0.4, threshold: 20} // override default settings
+  };
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
